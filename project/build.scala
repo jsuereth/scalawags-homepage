9d7b180bc7838b5f938aba37ff00aeba63e5cb76
Josh Suereth <joshua.suereth@gmail.com>
2013-01-18 13:04:29 -0500

    Bumped plugins for heroku

--- project/build.scala
+++ project/build.scala
@@ -1,11 +1,14 @@
 import sbt._
 import Keys._
 
+import com.typesafe.startscript.StartScriptPlugin
+
 object ScalawagsBuild extends Build {
 
   val root = (
     play.Project("scalawags-website", path = file(".")) 
     settings(scalaBinaryVersion := "2.10")
+    settings(StartScriptPlugin.startScriptForClassesSettings: _*)
   )
 
 }
